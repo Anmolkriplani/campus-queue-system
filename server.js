582ff406b94ef26703a4f2b1d1f65c48f9938bd9
Anmol kriplani <anmolkriplani2608@gmail.com>
2026-04-27 11:21:34 +0530

    Refactor server.js and update database connection path
    Signed-off-by: Anmol kriplani <anmolkriplani2608@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db");
+const connectDB = require("./db");
 
 const userRoutes = require("./routes/userRoutes");
 const tokenRoutes = require("./routes/tokenRoutes");
